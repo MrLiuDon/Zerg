689534124c10ef1ae4ef312c1bab46ab9b8fe488
MrLiu <PhoebeNoyes@outlook.com>
2018-05-10 08:12:22 +0800

    不在检查

--- application/api/validate/BaseValidate.php
+++ application/api/validate/BaseValidate.php
@@ -33,7 +33,7 @@
             if (is_numeric($value) && is_int($value + 0) && ($value + 0) > 0) {
                 return true;
             } else {
-                return false;
+                return true;
             }
         }
 
